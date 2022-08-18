ffb547b426432b7ce5e990c946550b90db34604e
lrdgz <developer0606@gmail.com>
2022-08-18 16:16:58 -0400

    Add routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,15 +27,27 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->configureRateLimiting();
+        $this->mapWebRoutes();
+        $this->mapApiRoutes();
+    }
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+    protected function mapWebRoutes()
+    {
+        foreach($this->centralDomains() as $domain) {
+            Route::middleware('web')->domain($domain)->group(base_path('routes/web.php'));
+        }
+    }
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
+    protected function mapApiRoutes()
+    {
+        foreach($this->centralDomains() as $domain) {
+            Route::middleware('api')->domain($domain)->prefix('api')->group(base_path('routes/api.php'));
+        }
+    }
+
+    protected function centralDomains()
+    {
+        return config('tenancy.central_domains');
     }
 
     /**
